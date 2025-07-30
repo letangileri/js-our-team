85048674d78f8ad50de4640bba32e0e281a2db16
letangileri <let.angileri@gmail.com>
2025-07-30 11:18:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png"
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png"
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png"
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png"
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png"
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png"
+  }
+];
+
+for(let i = 0; i < teamMembers.length; i++){
+}
+
+/* <div class="row g-0">
+  <div class="col-md-4">
+    <img src="./assets/img/female1.png" class="img-fluid rounded-start" alt="...">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div> */
